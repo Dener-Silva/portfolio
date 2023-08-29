32d800201d00c5b270811d6ac1eb6d226623732f
Dener-Silva <dener.siros@gmail.com>
2023-08-29 20:56:20 -0300

    Minor changes and fixed Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://test.com'
+    site: 'https://dener-silva.github.io',
+    base: '/portfolio',
 });
